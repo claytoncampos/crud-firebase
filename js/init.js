4353e4530f4c239d26adc0ab350c41cf3f9cb397
Clayton Campos <clayton.almeida.campos@gmail.com>
2020-09-28 16:05:08 -0300

    add files from desktop
    Add initial files

---
+++ js/init.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAvnHUybPgaXDo63oGADKvTvZD9b8AK-1U",
+    authDomain: "financeirowedding.firebaseapp.com",
+    databaseURL: "https://financeirowedding.firebaseio.com",
+    projectId: "financeirowedding",
+    storageBucket: "financeirowedding.appspot.com",
+    messagingSenderId: "1055862702543",
+    appId: "1:1055862702543:web:112859c96996ed4aa252ba",
+    measurementId: "G-6M9874EX4L"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
